0df64b41c578eec969963aff1cfcc5441044d44c
Levin Gsell <levin.gsell@bluewin.ch>
2023-11-22 15:45:12 +0100

    Added shadcn and tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, './src/') },
+      {
+        find: '@/components',
+        replacement: path.resolve(__dirname, './src/components/'),
+      },
+      { find: '@/lib', replacement: path.resolve(__dirname, './src/lib/') },
+    ],
+  },
 })
