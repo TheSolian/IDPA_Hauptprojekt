0f0ceae37a3208f2f02a5daff130606ef4339549
J.Vontobel.inf21 <j.vontobel.inf21@stud.bbbaden.ch>
2024-01-07 18:03:02 +0100

    added update and delete function

--- src/components/EditPage.tsx
+++ src/components/EditPage.tsx
@@ -40,6 +40,7 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 import { useNavigate, useParams } from 'react-router-dom'
+import { Checkbox } from './ui/checkbox'
 
 interface EditPageProps {}
 
@@ -67,10 +68,47 @@ const trueFalseFormSchema = z.object({
   }),
 })
 
+const multipleChoiceFormSchema = z.object({
+  category: z.string().min(2, {
+    message: 'Question must be at least 2 characters.',
+  }),
+  answer1: z.object({
+    answer: z.string().min(1, {
+      message: 'Answer must be at least 1 characters.',
+    }),
+    correct: z.boolean(),
+  }),
+  answer2: z.object({
+    answer: z.string().min(1, {
+      message: 'Answer must be at least 1 characters.',
+    }),
+    correct: z.boolean(),
+  }),
+  answer3: z.object({
+    answer: z.string().min(1, {
+      message: 'Answer must be at least 1 characters.',
+    }),
+    correct: z.boolean(),
+  }),
+  answer4: z.object({
+    answer: z.string().min(1, {
+      message: 'Answer must be at least 1 characters.',
+    }),
+    correct: z.boolean(),
+  }),
+  explanation: z.string().min(2, {
+    message: 'Explanation must be at least 2 characters.',
+  }),
+  question: z.string().min(2, {
+    message: 'Question must be at least 2 characters.',
+  }),
+})
+
 const EditPage: React.FC<EditPageProps> = ({}) => {
   const { id } = useParams()
   const navigate = useNavigate()
   const [question, setQuestion] = useState<Question>()
+  const [multiQuestion, setMultiQuestion] = useState<Question>()
   const [allCategories, setAllCategories] = useState<string[]>([])
   const [open, setOpen] = useState<boolean>(false)
   const [categoryName, setCategoryName] = useState<string>('')
@@ -99,7 +137,12 @@ const EditPage: React.FC<EditPageProps> = ({}) => {
         const docSnap = await getDoc(doc(db, 'questions', id))
 
         if (docSnap.exists()) {
-          setQuestion(docSnap.data() as Question)
+          const quest: Question = docSnap.data() as Question
+          if (quest.type === 'trueFalse') {
+            setQuestion(quest)
+          } else {
+            setMultiQuestion(quest)
+          }
         } else {
           console.log('No such document!')
         }
@@ -142,6 +185,52 @@ const EditPage: React.FC<EditPageProps> = ({}) => {
     },
   })
 
+  const multipleChoiceForm = useForm<z.infer<typeof multipleChoiceFormSchema>>({
+    resolver: zodResolver(multipleChoiceFormSchema),
+    defaultValues: {
+      category: multiQuestion?.categories,
+      answer1: {
+        answer: multiQuestion?.answers[0].title,
+        correct: multiQuestion?.answers[0].correct,
+      },
+      answer2: {
+        answer: multiQuestion?.answers[1].title,
+        correct: multiQuestion?.answers[1].correct,
+      },
+      answer3: {
+        answer: multiQuestion?.answers[2].title,
+        correct: multiQuestion?.answers[2].correct,
+      },
+      answer4: {
+        answer: multiQuestion?.answers[3].title,
+        correct: multiQuestion?.answers[3].correct,
+      },
+      explanation: multiQuestion?.explanation,
+      question: multiQuestion?.question,
+    },
+    values: {
+      category: multiQuestion?.categories,
+      answer1: {
+        answer: multiQuestion?.answers[0].title,
+        correct: multiQuestion?.answers[0].correct,
+      },
+      answer2: {
+        answer: multiQuestion?.answers[1].title,
+        correct: multiQuestion?.answers[1].correct,
+      },
+      answer3: {
+        answer: multiQuestion?.answers[2].title,
+        correct: multiQuestion?.answers[2].correct,
+      },
+      answer4: {
+        answer: multiQuestion?.answers[3].title,
+        correct: multiQuestion?.answers[3].correct,
+      },
+      explanation: multiQuestion?.explanation,
+      question: multiQuestion?.question,
+    },
+  })
+
   async function onSubmit(values: z.infer<typeof trueFalseFormSchema>) {
     if (values.correctAnswer?.answer === values.wrongAnswer?.answer) {
       console.log('The answers must not be the same.')
@@ -175,6 +264,56 @@ const EditPage: React.FC<EditPageProps> = ({}) => {
           console.error('Error updating document: ', error)
         }
       }
+    }
+  }
+
+  async function onMultipleChoiceSubmit(
+    values: z.infer<typeof multipleChoiceFormSchema>
+  ) {
+    if (
+      values.answer1.correct ||
+      values.answer2.correct ||
+      values.answer3.correct ||
+      values.answer4.correct
+    ) {
+      if (id) {
+        const questionRef = doc(db, 'questions', id)
+        const updatedData = {
+          answers: [
+            {
+              correct: values.answer1.correct,
+              title: values.answer1.answer,
+            },
+            {
+              correct: values.answer2.correct,
+              title: values.answer2.answer,
+            },
+            {
+              correct: values.answer3.correct,
+              title: values.answer3.answer,
+            },
+            {
+              correct: values.answer4.correct,
+              title: values.answer4.answer,
+            },
+          ],
+          categories: values.category,
+          explanation: values.explanation,
+          question: values.question,
+          type: 'multipleChoice',
+        }
+        try {
+          await updateDoc(questionRef, updatedData)
+          console.log('Document successfully updated')
+          navigate('/dashboard/questions')
+        } catch (error) {
+          console.error('Error updating document: ', error)
+        }
+      }
+      console.log(values)
+    } else {
+      setAnswerError(true)
+      console.log('flop')
     }
   }
 
@@ -414,7 +553,288 @@ const EditPage: React.FC<EditPageProps> = ({}) => {
           </form>
         </Form>
       ) : (
-        <div></div>
+        <Form {...multipleChoiceForm}>
+          <form
+            onSubmit={multipleChoiceForm.handleSubmit(onMultipleChoiceSubmit)}
+            className='space-y-8 w-1/2 mx-auto mt-8 mb-8'
+          >
+            <FormField
+              control={multipleChoiceForm.control}
+              name='category'
+              render={({ field }) => (
+                <FormItem className=' w-1/3'>
+                  <FormLabel>Category</FormLabel>
+                  <Select
+                    onValueChange={(value) => {
+                      field.onChange(value)
+                      openD(value)
+                    }}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder={multiQuestion?.categories} />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent className='h-48'>
+                      <SelectGroup>
+                        {allCategories.map((category) => (
+                          <SelectItem value={category}>{category}</SelectItem>
+                        ))}
+
+                        <SelectItem value='n'>Add a new category...</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={multipleChoiceForm.control}
+              name='question'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Question</FormLabel>
+                  <FormControl>
+                    <Input placeholder={multiQuestion?.question} {...field} />
+                  </FormControl>
+                  <FormDescription>Write your question here.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className='flex gap-8'>
+              <FormField
+                control={multipleChoiceForm.control}
+                name='answer1.answer'
+                render={({ field }) => (
+                  <FormItem className='w-1/3'>
+                    <FormLabel>Answer 1</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder={multiQuestion?.answers[1].title}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>Write your answer here.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={multipleChoiceForm.control}
+                name='answer1.correct'
+                render={({ field }) => (
+                  <FormItem className='flex flex-col gap-[12px] mt-[6px] items-start'>
+                    <FormLabel>Correct?</FormLabel>
+                    <FormControl>
+                      <Checkbox
+                        className='rounded-[3px] h-5 w-5'
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Choose whether this answer is correct.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className='flex gap-8'>
+              <FormField
+                control={multipleChoiceForm.control}
+                name='answer2.answer'
+                render={({ field }) => (
+                  <FormItem className='w-1/3'>
+                    <FormLabel>Answer 2</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder={multiQuestion?.answers[1].title}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>Write your answer here.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={multipleChoiceForm.control}
+                name='answer2.correct'
+                render={({ field }) => (
+                  <FormItem className='flex flex-col gap-[12px] mt-[6px] items-start'>
+                    <FormLabel>Correct?</FormLabel>
+                    <FormControl>
+                      <Checkbox
+                        className='rounded-[3px] h-5 w-5'
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Choose whether this answer is correct.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className='flex gap-8'>
+              <FormField
+                control={multipleChoiceForm.control}
+                name='answer3.answer'
+                render={({ field }) => (
+                  <FormItem className='w-1/3'>
+                    <FormLabel>Answer 3</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder={multiQuestion?.answers[2].title}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>Write your answer here.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={multipleChoiceForm.control}
+                name='answer3.correct'
+                render={({ field }) => (
+                  <FormItem className='flex flex-col gap-[12px] mt-[6px] items-start'>
+                    <FormLabel>Correct?</FormLabel>
+                    <FormControl>
+                      <Checkbox
+                        className='rounded-[3px] h-5 w-5'
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Choose whether this answer is correct.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className='flex gap-8'>
+              <FormField
+                control={multipleChoiceForm.control}
+                name='answer4.answer'
+                render={({ field }) => (
+                  <FormItem className='w-1/3'>
+                    <FormLabel>Answer 4</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder={multiQuestion?.answers[3].title}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>Write your answer here.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={multipleChoiceForm.control}
+                name='answer4.correct'
+                render={({ field }) => (
+                  <FormItem className='flex flex-col gap-[12px] mt-[6px] items-start'>
+                    <FormLabel>Correct?</FormLabel>
+                    <FormControl>
+                      <Checkbox
+                        className='rounded-[3px] h-5 w-5'
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      Choose whether this answer is correct.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <p
+              className='mb-4 text-sm font-medium text-destructive'
+              style={answerErrorStyle}
+            >
+              At least one answer must be correct.
+            </p>
+            <FormField
+              control={multipleChoiceForm.control}
+              name='explanation'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Explanation</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder={multiQuestion?.explanation}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Write an explanation for your students.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className='flex gap-8'>
+              <Button type='submit'>Update</Button>
+              <Dialog>
+                <DialogTrigger>
+                  <Button
+                    className='bg-destructive text-destructive-foreground hover:bg-red-800'
+                    type='button'
+                  >
+                    Delete
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle className='mb-4'>Delete?</DialogTitle>
+                    <DialogDescription>
+                      <Button
+                        onClick={deleteQuestion}
+                        className='bg-destructive text-destructive-foreground hover:bg-red-800'
+                      >
+                        Yes!
+                      </Button>
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
+            </div>
+            <Dialog open={open} onOpenChange={setOpen}>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Add a new category</DialogTitle>
+                  <DialogDescription>
+                    <Input
+                      className='my-4'
+                      name='categoryName'
+                      placeholder='Category name...'
+                      onChange={(e) => setCategoryName(e.target.value)}
+                    />
+                    <p
+                      className='mb-4 text-sm font-medium text-destructive'
+                      style={errorStyle}
+                    >
+                      Category already exists or isn't long enough
+                    </p>
+                    <Button onClick={addCat}>Create category</Button>
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          </form>
+        </Form>
       )}
     </>
   )
